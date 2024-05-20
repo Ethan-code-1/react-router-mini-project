f4a6434506b749aa38b18fcb47dfef480b37a086
Ethan-code-1 <ebo4dq@virginia.edu>
2024-05-20 14:52:59 -0400

    Finished most of the ui

--- vite-project/src/roots/Projects.jsx
+++ vite-project/src/roots/Projects.jsx
@@ -1,14 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar.jsx"; 
+import { Box, Button, ListItemText, Typography, Container, List, ListItem } from '@mui/material';
+
+import "../styles/projects.css";
 
 
 export const Projects = () => {
 	return (
 		<div>
-                        <Navbar />
+            <Navbar />
 
-			<h1>Projects</h1>
+            <Container className="projects-container">
+            <Box className="project-item">
+                <Typography variant="h6" className="project-title">
+                Personal Project, Course Registration Hub
+                </Typography>
+                <List className="project-details">
+                <ListItem>
+                    <ListItemText primary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+                </ListItem>
+                </List>
+            </Box>
+            <Box className="project-item">
+                <Typography variant="h6" className="project-title">
+                Personal Project, Photo Hunt UVA
+                </Typography>
+                <List className="project-details">
+                <ListItem>
+                    <ListItemText primary="Dignissim sodales ut eu sem. Nunc sed velit dignissim sodales ut eu sem integer vitae. Nunc id cursus metus aliquam. In mollis nunc sed id semper risus in hendrerit gravida. Congue quisque egestas diam in arcu. Augue eget arcu dictum varius duis. Eget magna fermentum iaculis eu. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Donec adipiscing tristique risus nec feugiat in fermentum." />
+                </ListItem>
+                </List>
+            </Box>
+            <Box className="project-item">
+                <Typography variant="h6" className="project-title">
+                Personal Project, Page Table Implementation
+                </Typography>
+                <List className="project-details">
+                <ListItem>
+                    <ListItemText primary="Sed turpis tincidunt id aliquet. Viverra accumsan in nisl nisi scelerisque eu. Aliquam purus sit amet luctus venenatis lectus magna fringilla. Non diam phasellus vestibulum lorem sed risus ultricies. Nulla facilisi etiam dignissim diam quis enim. Eu feugiat pretium nibh ipsum consequat nisl. Sagittis aliquam malesuada bibendum arcu vitae. Maecenas sed enim ut sem viverra aliquet eget sit amet. Magna sit amet purus gravida quis blandit. Sed faucibus turpis in eu mi bibendum. Magna fringilla urna porttitor rhoncus dolor. Varius sit amet mattis vulputate. Velit dignissim sodales ut eu sem integer vitae justo." />
+                </ListItem>
+                </List>
+            </Box>
+        </Container>
 			
 		</div>
 	);
--- vite-project/src/roots/Root.jsx
+++ vite-project/src/roots/Root.jsx
@@ -17,6 +17,7 @@ export const Root = () => {
                     <p className = "biotext">
                     Ethan is deeply passionate about the intersection of technology and business. As a Computer Science major and Data Science minor, he has experience with tools used in both industries. Ethan enjoys the rapid pace of innovation within both subjects and loves being surrounded by other forward-thinking individuals. In the future, Ethan wants to dive deeper into the design decisions that underpin creating great software, mastering tools such as AWS and React in the process.
                     </p>
+                    <p className = "biotext">Elementum eu facilisis sed odio morbi. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. At ultrices mi tempus imperdiet nulla malesuada. Maecenas ultricies mi eget mauris pharetra et ultrices neque ornare. Malesuada fames ac turpis egestas integer eget aliquet. Dui vivamus arcu felis bibendum ut. Elit eget gravida cum sociis natoque penatibus et magnis dis. Egestas tellus rutrum tellus pellentesque eu tincidunt. Accumsan lacus vel facilisis volutpat est. Turpis massa tincidunt dui ut ornare lectus sit amet. Duis at tellus at urna condimentum mattis pellentesque. In hac habitasse platea dictumst vestibulum. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. Placerat in egestas erat imperdiet sed euismod.</p>
                 </div>
             </div>
 		</div>
